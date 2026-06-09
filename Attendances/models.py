0f783dd80b9dc66847f1e7e414f7ad4d52642f03
SengQT <hoksengxd011@gmail.com>
2026-06-09 21:09:32 +0700

    commit attendances model

--- Attendances/models.py
+++ Attendances/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from Classrooms.models import ClassRoom
+from Subjects.models import Subject
+from Teachers.models import Teacher
 
-# Create your models here.
+class Attendance(models.Model):
+    # One session = one class, one subject, one teacher, one date
+    classroom = models.ForeignKey(
+        ClassRoom,
+        on_delete=models.PROTECT,
+        related_name='attendances'
+    )
+    subject = models.ForeignKey(
+        Subject,
+        on_delete=models.PROTECT,
+        related_name='attendances'
+    )
+    teacher = models.ForeignKey(
+        Teacher,
+        on_delete=models.PROTECT,
+        related_name='attendances'
+    )
+
+    date = models.DateField()
+    notes = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # Can't have two sessions for same class+subject on same day
+        unique_together = ['classroom', 'subject', 'date']
+        ordering = ['-date']
+
+    def __str__(self):
+        return f"{self.classroom} | {self.subject} | {self.date}"
