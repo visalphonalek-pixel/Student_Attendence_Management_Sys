df4de9d7e9dc056bf531d2772e23d9b9b8088269
SengQT <hoksengxd011@gmail.com>
2026-06-10 01:12:02 +0700

    add classroom templates edit all the files with double < (<<nav) edit classroom urls.py from classes to classrooms

--- Classrooms/urls.py
+++ Classrooms/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("classes/", views.classroom_list, name="classroom_list"),
-    path("classes/create/", views.classroom_create, name="classroom_create"),
-    path("classes/<int:pk>/", views.classroom_detail, name="classroom_detail"),
-    path("classes/<int:pk>/edit/", views.classroom_edit, name="classroom_edit"),
-    path("classes/<int:pk>/delete/", views.classroom_delete, name="classroom_delete"),
+    path("classrooms/", views.classroom_list, name="classroom_list"),
+    path("classrooms/create/", views.classroom_create, name="classroom_create"),
+    path("classrooms/<int:pk>/", views.classroom_detail, name="classroom_detail"),
+    path("classrooms/<int:pk>/edit/", views.classroom_edit, name="classroom_edit"),
+    path("classrooms/<int:pk>/delete/", views.classroom_delete, name="classroom_delete"),
 ]
