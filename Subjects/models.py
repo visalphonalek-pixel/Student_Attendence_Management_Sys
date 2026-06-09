290f66079c3ddbf280ffec40dbda992204b4db6d
SengQT <hoksengxd011@gmail.com>
2026-06-09 21:09:07 +0700

    commit subjects  model

--- Subjects/models.py
+++ Subjects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
